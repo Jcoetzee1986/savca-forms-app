a77753f827637280349217f05cbde962fc1d58d9
Jcoetzee1986 <coetzee.jj@gmail.com>
2019-08-01 12:28:39 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,16 +5,13 @@ import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { WizardComponent } from './wizard/wizard.component';
 import { ResetMailerComponent } from './auth/reset-mailer/reset-mailer.component';
+import { ResetPasswordComponent } from './auth/reset-password/reset-password.component';
 import { FMDPApplicationModule } from './fmdp-application/fmdp-application.module';
 import { AuthModule } from './auth/auth.module';
 
 const routes: Routes = [
 
-  { path: 'auth', loadChildren: () => AuthModule },
-  /* {
-    path: 'auth',
-    loadChildren: './auth/auth.module#AuthModule'
-  }, */
+  
   {
     path: '#',
     component: WizardComponent
@@ -28,6 +25,17 @@ const routes: Routes = [
     component: WizardComponent
   },
   {
+    path: 'mailer',
+    component: ResetMailerComponent
+  },
+  {
+    path: 'reset',
+    component: ResetPasswordComponent
+  },
+  {
+    path: 'auth', loadChildren: () => AuthModule 
+  },
+  {
     path: 'fmdp-application',
     loadChildren: './fmdp-application/fmdp-application.module#FMDPApplicationModule'
   }
@@ -38,6 +46,8 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forRoot(routes)
-  ]
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
